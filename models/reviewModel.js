008f8516e4910bb971a2236487d0b6ff0c30dd63
shemamoses <mosesshema1@gmail.com>
2023-05-21 17:51:26 +0200

    jj

--- models/reviewModel.js
+++ models/reviewModel.js
@@ -1,36 +1,42 @@
 const mongoose = require('mongoose');
 
-const reviewSchema = new mongoose.Schema({
-  review: {
-    type: String,
-    required: [true, 'review cannot be empty']
+const reviewSchema = new mongoose.Schema(
+  {
+    review: {
+      type: String,
+      required: [true, 'review cannot be empty']
+    },
+    rating: {
+      type: Number,
+      min: 1,
+      max: 5,
+      required: true
+    },
+    createdAt: {
+      type: date,
+      default: Date.now,
+      required: true
+    },
+    tour: {
+      type: mongoose.Schema.ObjectId,
+      ref: Tour,
+      required: [true, 'Review must belong to a tour']
+    },
+    refUse: {
+      type: String,
+      required: true
+    },
+    user: {
+      type: mongoose.Schema.ObjectId,
+      ref: 'User',
+      required: [true, 'Review must belong to a user']
+    }
   },
-  rating: {
-    type: Number,
-    min: 1,
-    max: 5,
-    required: true
-  },
-  createdAt: {
-    type: date,
-    default: Date.now,
-    required: true
-  },
-  tour: {
-    type: mongoose.Schema.ObjectId,
-    ref: Tour,
-    required: [true, 'Review must belong to a tour']
-  },
-  refUse: {
-    type: String,
-    required: true
-  },
-  user: {
-    type: mongoose.Schema.ObjectId,
-    ref: 'User',
-    required: [true, 'Review must belong to a user']
+  {
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true }
   }
-});
+);
 const Review = mongoose.model('Review', reviewSchema);
 
 module.exports = Review;
