61abcba7f4a3818c0ced86b26f2e9888e54e85e8
shemamoses <mosesshema1@gmail.com>
2023-05-21 16:16:57 +0200

    .

--- models/reviewModel.js
+++ models/reviewModel.js
@@ -5,11 +5,38 @@ const reviewSchema = new mongoose.Schema({
     type: String,
     required: [true, 'review cannot be empty']
   },
-  rating: { type: Number, min: 1, max: 5, required: true },
-  createdAt: { type: String, required: true },
-  refTour: { type: String, required: true },
-  refUse: { type: String, required: true }
-});
+  rating: {
+    type: Number,
+    min: 1,
+    max: 5,
+    required: true
+  },
+  createdAt: {
+    type: date,
+    default: Date.now,
+    required: true
+  },
+  tour: {
+    type: mongoose.Schema.ObjectId,
+    ref: Tour,
+    required: [true, 'Review must belong to a tour']
+  },
+  refUse: {
+    type: String,
+    required: true
+  },
+  user: {
+    type: mongoose.Schema.ObjectId,
+    ref: 'User',
+    required: [true, 'Review must belong to a user']
+  },
+  {
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true }
+  }
+})
+const Review = mongoose.model('Review', reviewSchema);
 
-const User = mongoose.model('User', reviewSchema);
+module.exports = Review;
+
 // review / rating / createdAt / ref to tour / ref to use
