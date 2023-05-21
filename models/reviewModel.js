e702674450fa75f1b0391e35520c2c25cbf94e3d
shemamoses <mosesshema1@gmail.com>
2023-05-21 15:36:28 +0200

    .

--- models/reviewModel.js
+++ models/reviewModel.js
@@ -1,2 +1,15 @@
+const mongoose = require('mongoose');
 
+const reviewSchema = new mongoose.Schema({
+  review: {
+    type: String,
+    required: true
+  },
+  rating: { type: String, required: true },
+  createdAt: { type: String, required: true },
+  refTour: { type: String, required: true },
+  refUse: { type: String, required: true }
+});
+
+const User = mongoose.model('User', reviewSchema);
 // review / rating / createdAt / ref to tour / ref to use
