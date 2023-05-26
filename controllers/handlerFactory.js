9bf2892d3d742c46c2192ea95442ee7019f0da6a
shemamoses <mosesshema1@gmail.com>
2023-05-26 17:05:57 +0200

    ..

---
+++ controllers/handlerFactory.js
@@ -0,0 +1,11 @@
+exports.deleteOne = Model =>
+  catchAsync(async (req, res, next) => {
+    const doc = await Model.findByIdAndDelete(req.params.id);
+    if (!doc) {
+      return next(new AppError('No document found with that ID', 404));
+    }
+    res.status(204).json({
+      status: 'success',
+      data: null
+    });
+  });
