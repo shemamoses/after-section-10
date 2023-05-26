a4c68cf0799c8f844bbe504c4993b5848458cc17
shemamoses <mosesshema1@gmail.com>
2023-05-26 16:47:59 +0200

    nested routes

--- controllers/reviewController.js
+++ controllers/reviewController.js
@@ -4,7 +4,9 @@ const catchAsync = require('./../utils/catchAsync');
 
 //get all reviews
 exports.getAllReviews = catchAsync(async (req, res, next) => {
-  const reviews = await Review.find();
+  let filter = {};
+  if (req.params.tourId) filter = { tour: req.params.tourId };
+  const reviews = await Review.find(filter);
   return res.status(200).json({
     status: 'success',
     data: {
@@ -17,6 +19,9 @@ exports.getAllReviews = catchAsync(async (req, res, next) => {
 
 //create a review
 exports.createReview = catchAsync(async (req, res, next) => {
+  //allow nested routes
+  if (!req.body.tour) req.body.tour = req.params.tourId;
+  if (!req.body.user) req.body.user = req.user.id;
   const newReview = await Review.create(req.body);
   res.status(201).json({
     status: 'success',
